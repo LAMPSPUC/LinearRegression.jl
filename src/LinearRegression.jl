faf5f297aa6eb20111de09868a37b51d67a110a1
Rafaela <rafaelamaribeiro@gmail.com>
2020-03-30 09:59:18 -0300

    Suggested changes done

--- src/LinearRegression.jl
+++ src/LinearRegression.jl
@@ -3,8 +3,9 @@ module LinearRegression
 # Function that estimates the coefficients for a multiple linear regression model.
 # It receives the data, y as a vector and X as a matrix.
 
-
-function add_column(x)
+"""
+function add_column(x::Matrix{Float64})
+    # function thar adds a column of ones in the matrix
     lin,col = size(x)
     X = ones(lin,(col+1))
     for i in 1:col
@@ -12,34 +13,41 @@ function add_column(x)
     end
     return X
 end
+"""
 
-function find_beta_hat(x, y)
-    X = add_column(x)
-    beta_hat = ((X'*X)^-1)*(X'y)
-    return beta_hat
+function beta_hat(X::Matrix{Float64}, y::Vector{Float64})
+    # Book implementation for estimation of parameters using the mathod of ordinary least squares method
+    return ((X'*X)^-1)*(X'y)
 end
 
-function calculate_residual(X, y, beta_hat)
-    ϵ = y-(X*beta_hat)
-    return ϵ
+
+function residuals(X::Matrix{Float64}, y::Vector{Float64}, beta_hat::Vector{Float64})
+    # Book implementation for calculating residuals (y = Xβ + ϵ)
+    return y-(X*beta_hat)
 end
 
-function calculateR2(X, y, beta_hat)
-    SSr = beta_hat'*X'*y - ((sum(y))^2)/(length(y))
-    #SSe = y'*y - beta_hat'*X'*y
+
+function SSe(X::Matrix{Float64}, y::Vector{Float64}, beta_hat::Vector{Float64})
+    # Sum of squares of errors (or residuals) formula
+    return y'*y - beta_hat'*X'*y
+end
+
+
+function R2(X::Matrix{Float64}, y::Vector{Float64}, beta_hat::Vector{Float64})
+    # calculates the R² (coefficient of determination) of a estimation based on the book implementation
+    SSE = SSe(X, y, beta_hat)
     Syy = y'*y - ((sum(y))^2)/(length(y))
-    R2 = SSr/Syy
-    #R2 = 1 - SSe/Syy
-    return R2
+    return 1 - SSE/Syy
 end
 
-function calculateMSE(X, y, beta_hat) #σ²
-    SSe = y'*y - beta_hat'*X'*y
+
+function MSE(X::Matrix{Float64}, y::Vector{Float64}, beta_hat::Vector{Float64}) #σ²
+    # calculates the MSE (mean square error) of a estimation based on the book implementation
+    SSE = SSe(X, y, beta_hat)
     p = length(beta_hat) - 1
-    print(length(beta_hat))
-    MSE = SSe/(length(y) - p)
-    return MSE
+    return SSE/(length(y) - p)
 end
+
 """
 function calulate_p_value(X, beta_hat, MSE)
     #p_value = []
@@ -53,31 +61,35 @@ function calulate_p_value(X, beta_hat, MSE)
     #return p_value
 end
 """
-function linear_regression(x, y)
-    X = add_column(x)
+
+function linreg(X::Matrix{Float64}, y::Vector{Float64})
+    #X = add_column(X)
     # finding beta_hat
-    beta_hat = find_beta_hat(x, y)
+    betahat = beta_hat(X, y)
     # calculating the residuals
-    ϵ = calculate_residual(X, y, beta_hat)
+    ϵ = residuals(X, y, betahat)
     # calculating R²
-    R² = calculateR2(X, y, beta_hat)
+    R² = R2(X, y, betahat)
     # calculating MSE (σ²)
-    MSE = calculateMSE(X, y, beta_hat)
+    MSe = MSE(X, y, betahat)
     # calculating p-value
     #p_value = calulate_p_value(X, beta_hat, MSE)
-    results = Dict("beta_hat" => beta_hat, "R²" => R², "MSE (σ²)" => MSE) #, "p-value" => p_value )
+    results = Dict("beta_hat" => betahat, "R²" => R², "MSE (σ²)" => MSe) #, "p-value" => p_value )
     return results
 end
 
+
 X = rand(100, 3)
 y = rand(100)
+
 """
 Xn = add_column(X)
-beta_hat = find_beta_hat(X, y)
+betahat = beta_hat(X, y)
 MSE = calculateMSE(Xn, y, beta_hat)
 
 calulate_p_value(Xn, beta_hat, MSE)
 """
-linear_regression(X, y)
+
+linreg(X, y)
 
 end # module
