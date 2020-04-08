26d783062f62cb15b86451c45036c6205f5c2978
Rafaela <rafaelamaribeiro@gmail.com>
2020-04-08 12:55:32 -0300

    First example

---
+++ Examples.jl/generic _straight_line.jl
@@ -0,0 +1,33 @@
+
+using CSV
+
+# Let X be a matrix and y a vector as in the following example
+
+data = CSV.read("example.csv"; delim=';', decimal=',')
+
+r, c = size(data)
+x = data[:, 2:c]
+
+function add_column(x)
+    row,col = size(x)
+    X = ones(row,(col+1))
+    for i in 1:col
+        X[:,i+1] = x[:,i]
+    end
+    return X
+end
+
+X = add_column(x)
+
+df = data[:, 1:2]
+y = df[!, 1]
+
+# Estimate a multiple linear regression model
+
+reg = linreg(y, X)
+
+
+
+
+
+
